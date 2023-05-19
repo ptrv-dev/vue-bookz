64ab4440e41e7ce9e4060cc3a88b3ddb3405d575
ptrv-dev <petrov99119@gmail.com>
2023-05-19 10:41:17 +0300

    Display books on Home Page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,37 @@
+<script>
+import AppBook from '../components/AppBook.vue';
+
+export default {
+  name: 'HomePage',
+  components: { AppBook },
+  computed: {
+    books() {
+      return this.$store.state.books;
+    },
+  },
+  methods: {
+    handleBookClick(_id) {
+      this.$router.push('/book/' + _id);
+    },
+  },
+};
+</script>
+
 <template>
-  <div class="container">
-    <h1>Home Page</h1>
+  <div class="container mt-4">
+    <h1 class="text-white text-2xl font-bold">
+      {{ books.length ? 'List of your books:' : 'Your books list is empty...' }}
+    </h1>
+    <div v-if="books.length" class="grid grid-cols-1 sm:grid-cols-2 mt-4 gap-4">
+      <app-book
+        v-for="book in books"
+        @click="handleBookClick(book._id)"
+        :title="book.title"
+        :author="book.author"
+        :pagesCount="Number(book.pages)"
+        :created="book.created"
+        :updated="book.updated"
+      />
+    </div>
   </div>
 </template>

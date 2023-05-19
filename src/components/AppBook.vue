edcc38db734a251044f0661473e884612c2acbb0
ptrv-dev <petrov99119@gmail.com>
2023-05-19 10:41:01 +0300

    AppBook component and AppProgressBar component

---
+++ src/components/AppBook.vue
@@ -0,0 +1,97 @@
+<script>
+import AppProgressBar from './AppProgressBar.vue';
+
+export default {
+  name: 'AppBook',
+  components: { AppProgressBar },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    pagesCount: {
+      type: Number,
+      required: true,
+    },
+    created: {
+      type: Number,
+      required: true,
+    },
+    updated: {
+      type: Number,
+      required: true,
+    },
+    read: {
+      type: Number,
+      default: 0,
+    },
+  },
+  emits: {
+    click: null,
+  },
+  computed: {},
+  methods: {
+    dateFormat(unix) {
+      const months = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+      ];
+      const date = new Date(unix);
+      const day = date.getDate();
+      const month = months[date.getMonth()];
+      const hour = date.getHours();
+      const minutes = date.getMinutes();
+      return `${day} ${month}, ${hour}:${minutes}`;
+    },
+  },
+};
+</script>
+<template>
+  <div
+    @click="$emit('click')"
+    class="bg-white p-4 rounded-2xl flex flex-col gap-4 cursor-pointer transition-transform hover:-translate-y-2"
+  >
+    <div class="flex flex-col">
+      <span class="uppercase text-xs leading-none">Book title</span>
+      <p class="text-lg">{{ title }}</p>
+    </div>
+    <div class="flex gap-4">
+      <div class="flex flex-col flex-1">
+        <span class="uppercase text-xs leading-none">Author</span>
+        <p class="text-lg">{{ author }}</p>
+      </div>
+      <div class="flex flex-col flex-1">
+        <span class="uppercase text-xs leading-none">Pages count</span>
+        <p class="text-lg">{{ pagesCount }}</p>
+      </div>
+    </div>
+    <div class="flex gap-4">
+      <div class="flex flex-col flex-1">
+        <span class="uppercase text-xs leading-none">Created date</span>
+        <p class="text-lg">{{ dateFormat(created) }}</p>
+      </div>
+      <div class="flex flex-col flex-1">
+        <span class="uppercase text-xs leading-none">Last update</span>
+        <p class="text-lg">{{ dateFormat(updated) }}</p>
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <span class="uppercase text-xs leading-none mb-2">Progress</span>
+      <app-progress-bar :currentValue="read" :maxValue="pagesCount" />
+    </div>
+  </div>
+</template>
---
+++ src/components/AppProgressBar.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  name: 'ProgressBar',
+  props: {
+    currentValue: {
+      type: Number,
+      required: true,
+    },
+    maxValue: {
+      type: Number,
+      required: true,
+    },
+  },
+  emits: {},
+  computed: {
+    percent() {
+      return (this.currentValue / this.maxValue) * 100;
+    },
+    displayPercent() {
+      return Math.trunc(this.percent);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="rounded-lg flex bg-light h-4 w-full items-center relative">
+    <div
+      class="bg-primary h-4 rounded-lg flex text-xs text-white font-medium items-center justify-center"
+      :style="{ width: this.percent + '%' }"
+    >
+      <template v-if="percent >= 25"> {{ displayPercent + '%' }}</template>
+    </div>
+    <span
+      v-if="percent < 25"
+      class="text-xs text-primary font-medium absolute w-full text-center"
+      >{{ displayPercent + '%' }}</span
+    >
+  </div>
+</template>

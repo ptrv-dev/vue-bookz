97362b2af5b81abc0374a7f5e1a8aed7dec3b60c
ptrv-dev <petrov99119@gmail.com>
2023-05-20 10:13:22 +0300

    Book delete functionality

--- src/pages/BookPage.vue
+++ src/pages/BookPage.vue
@@ -46,6 +46,20 @@ export default {
       });
       this.pagesCount = '';
     },
+    handleDelete() {
+      if (
+        !window.confirm(
+          'Do you really want to delete this book from your list?'
+        )
+      )
+        return;
+      this.$store.dispatch('removeBook', { _id: this.paramId });
+      this.$store.dispatch('addNotification', {
+        title: 'Success',
+        text: 'Book was successfully deleted',
+      });
+      this.$router.push('/');
+    },
   },
   watch: {
     pagesCount() {
@@ -79,7 +93,28 @@ export default {
             />
           </svg>
         </button>
-        <h1 class="text-white font-bold text-2xl">Detailed information:</h1>
+        <h1 class="text-white font-bold text-2xl flex-1">
+          Detailed information:
+        </h1>
+        <button
+          @click="handleDelete"
+          class="bg-white p-2 rounded-full text-primary transition-colors hover:text-red-600"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+          </svg>
+        </button>
       </div>
       <div class="flex flex-col bg-white text-dark p-8 rounded-2xl gap-4">
         <div class="flex flex-col">
